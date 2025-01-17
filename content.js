61c0b91f428672408a044261ece4ba97e9227d6d
Rohan Satkar <coderxrohan@gmail.com>
2025-01-17 15:14:47 +0530

    Create content.js

---
+++ content.js
@@ -0,0 +1,6 @@
+(function enablePiP() {
+  const video = document.querySelector("video");
+  if (video && document.pictureInPictureElement !== video) {
+    video.requestPictureInPicture().catch(console.error);
+  }
+})();
